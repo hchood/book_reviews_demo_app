7c0440da80b0afc0a6e8cd6b41cfa07b2466df1a
Helen Hood <helen.c.hood@gmail.com>
2014-09-23 10:51:10 -0400

    Happy path passing for creating a user

--- spec/features/user_creates_account_spec.rb
+++ spec/features/user_creates_account_spec.rb
@@ -20,6 +20,8 @@ feature "User creates an account", %Q{
     visit root_path
     click_on "Sign up"
 
+    fill_in "First name", with: user.first_name
+    fill_in "Last name", with: user.last_name
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     fill_in "Password confirmation", with: user.password
